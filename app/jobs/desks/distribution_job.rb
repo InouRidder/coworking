53338151b72533e762a7632948f62c9b1b4258bf
Inou Ridder <inouridder@gmail.com>
2020-03-16 21:16:24 +0100

    ft: add initial contract renewal job, add specs for expectancy

--- app/jobs/desks/distribution_job.rb
+++ app/jobs/desks/distribution_job.rb
@@ -1,11 +1,16 @@
-class Desks::DistributionJob < ApplicationJob
-  queue_as :default
+# frozen_literal_string: true
 
-  def perform
-    # I realise this is heavy on the DB, but seems the least prone to bug
-    while Desk.any_available? && Request.confirmed.any?
-      Request.confirmed.in_waiting_list_order.each do |request|
-        request.accept!
+module Desks
+  # Job in charge of distributing free desks to confirmed requests
+  class DistributionJob < ApplicationJob
+    queue_as :default
+
+    def perform
+      # I realise this is heavy on the DB, but seems the least prone to bug
+      while Desk.any_available? && Request.confirmed.any?
+        Request.confirmed.in_waiting_list_order.each do |request|
+          request.accept!
+        end
       end
     end
   end
