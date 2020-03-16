535f0cc317e836168e50e41af9a66a72e4ba81e7
Inou Ridder <inouridder@gmail.com>
2020-03-16 12:58:17 +0100

    ft: add registrations factory

--- spec/factories/registrations.rb
+++ spec/factories/registrations.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :registration do
-    
+    email { Faker::Internet.email }
+    password { 'password' }
+    name { Faker::Name.name }
+    bio { Faker::GreekPhilosophers.quote }
+    phone { '+3312345678' }
   end
 end
