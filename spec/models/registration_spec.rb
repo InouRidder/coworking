a055fc42aecb622a4ee52e42ff8812d98f0d8f2b
Inou Ridder <inouridder@gmail.com>
2020-03-17 13:02:45 +0100

    tests: for registration data validations

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -5,47 +5,47 @@ RSpec.describe Registration, type: :model do
 
   describe "Validations" do
     it "is valid with valid attributes" do
-      expect(subject).to be_valid
+      expect(registration).to be_valid
     end
 
     it "is not valid without a password" do
-      subject.password = nil
-      expect(subject).to_not be_valid
+      registration.password = nil
+      expect(registration).to_not be_valid
     end
 
     it "is not valid without an email" do
-      subject.email = nil
-      expect(subject).to_not be_valid
+      registration.email = nil
+      expect(registration).to_not be_valid
     end
 
     it 'is not valid with an invalid email' do
-      subject.email = 'not-an-email'
-      expect(subject).to_not be_valid
+      registration.email = 'not-an-email'
+      expect(registration).to_not be_valid
     end
 
     it "is not valid without a phone number" do
-      subject.phone = nil
-      expect(subject).to_not be_valid
+      registration.phone = nil
+      expect(registration).to_not be_valid
     end
 
     it 'is not valid with an invalid phone number' do
-      subject.phone = '+32123'
-      expect(subject).to_not be_valid
+      registration.phone = '+32123'
+      expect(registration).to_not be_valid
     end
 
     it 'is not valid without a name' do
-      subject.name = nil
-      expect(subject).to_not be_valid
+      registration.name = nil
+      expect(registration).to_not be_valid
     end
 
     it 'is not valid without a bio' do
-      subject.bio = nil
-      expect(subject).to_not be_valid
+      registration.bio = nil
+      expect(registration).to_not be_valid
     end
 
     it 'is not valid with a short bio' do
-      subject.bio = 'too short'
-      expect(subject).to_not be_valid
+      registration.bio = 'too short'
+      expect(registration).to_not be_valid
     end
   end
 end
