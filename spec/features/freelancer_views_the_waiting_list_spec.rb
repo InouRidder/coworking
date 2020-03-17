91f4c0413840f52899428af81a479d1f40b7c9ff
Inou Ridder <inouridder@gmail.com>
2020-03-17 18:20:05 +0100

    fix: remove commented test

--- spec/features/freelancer_views_the_waiting_list_spec.rb
+++ spec/features/freelancer_views_the_waiting_list_spec.rb
@@ -39,16 +39,4 @@ feature 'Freelancer views the waiting list' do
       expect(page).to have_content(request.registration.email)
     end
   end
-
-  # scenario 'invalid phone' do
-  #   fill_in_form_with 'cool@beans.fr', 'Inou', 'Freelancer web dev learning french and code!', '+33212'
-
-  #   expect(page).to have_content('Phone is invalid')
-  # end
-
-  # scenario 'invalid bio' do
-  #   fill_in_form_with 'cool@beans.fr', 'Inou', '', '+33712121212'
-
-  #   expect(page).to have_content('Bio is too short')
-  # end
 end
