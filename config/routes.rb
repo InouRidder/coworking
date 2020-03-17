ba0e226ef5f26d4460b831c2631ca6ba967a3b22
Inou Ridder <inouridder@gmail.com>
2020-03-17 11:13:27 +0100

    ft: add fields to sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  resources :registrations, only: %i[new create]
+
   namespace :requests  do
     get :confirmations, to: 'confirmations#update' # no patch requests in mailers
   end
