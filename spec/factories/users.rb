3a6dca025f8a1eb01bf3d4bf44512d6fd9db4bbf
Inou Ridder <inouridder@gmail.com>
2020-03-17 10:15:57 +0100

    tests: add spec for renew contract job

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
     registration { association(:registration) }
+    status { 'active' }
   end
 end
--- spec/jobs/contracts/renew_job_spec.rb
+++ spec/jobs/contracts/renew_job_spec.rb
@@ -1,11 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Contracts::RenewJob, type: :job do
+  let(:job) { Contracts::RenewJob }
+  let(:contract_to_be_renewed) { create(:contract, end_date: Date.today, status: 'paid') }
+  let(:active_contract) { create(:contract, end_date: Date.today - 1.week, status: 'unpaid') }
+  let(:expired_contract) { create(:contract, status: 'paid', end_date: Date.today + 1.week) }
+
   describe '#perform' do
     it 'should renew contracts of users that are still active' do
+      contract_to_be_renewed
+
+      expect {
+        job.perform_now
+      }.to change { contract_to_be_renewed.user.contracts.count }.by(1)
     end
 
-    it 'should not renew contracts for users that went inactive' do
+    it 'should expire paid contracts that are passed the end_date' do
+      expired_contract
+      job.perform_now
+
+      expect(expired_contract.reload.expired?).to be_truthy
+    end
+
+    it 'should not renew active contracts' do
+      active_contract
+
+      expect {
+        job.perform_now
+      }.to change { active_contract.user.contracts.count }.by(0)
+      expect(active_contract.reload.expired?).to be_falsy
+    end
+
+    it 'should not renew expired contracts' do
+      expired_contract
+
+      expect {
+        job.perform_now
+      }.to change { expired_contract.user.contracts.count }.by(0)
+    end
+
+    it 'should not renew contracts of inactive users' do
+      user = create(:user, status: 'inactive')
+      create(:contract, user: user, end_date: Date.today)
+
+      expect {
+        job.perform_now
+      }.to change { user.contracts.count }.by(0)
     end
 
     it 'should free the desk to available again' do
