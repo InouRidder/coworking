973cee902c3102f068ce566ed0487682458fcdf6
Inou Ridder <inouridder@gmail.com>
2020-03-17 10:58:12 +0100

    tests: for email for the reminder, including the position in the waiting list

--- spec/mailers/request_spec.rb
+++ spec/mailers/request_spec.rb
@@ -1,18 +1,21 @@
 require "rails_helper"
 
 RSpec.describe RequestMailer, type: :mailer do
-  # describe "send_confirm_interest" do
-  #   let(:mail) { RequestMailer.send_confirm_interest }
+  let(:request) { create(:request, status: 'confirmed') }
+  let(:position) { Request.confirmed.in_waiting_list_order.index(request) + 1}
+  let(:mail) { RequestMailer.with(request: request, position: position).send_confirm_interest }
+  describe "send_confirm_interest" do
 
-  #   it "renders the headers" do
-  #     expect(mail.subject).to eq("Send confirm interest")
-  #     expect(mail.to).to eq(["to@example.org"])
-  #     expect(mail.from).to eq(["from@example.com"])
-  #   end
+    it "renders the headers" do
+      expect(mail.subject).to eq('Are you still interested?')
+      expect(mail.to).to eq([request.registration.email])
+      expect(mail.from).to eq(['contact@coworking.com'])
+    end
 
-  #   it "renders the body" do
-  #     expect(mail.body.encoded).to match("Hi")
-  #   end
-  # end
+    it "renders the body (including the waitling list position)" do
+      request
+      expect(mail.body.encoded).to match("current position in qeue: #{position}")
+    end
+  end
 
 end
