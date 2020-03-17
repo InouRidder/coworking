4c9bbc0d1623aef3a38c996dfd278d7249dbfb14
Inou Ridder <inouridder@gmail.com>
2020-03-17 11:26:56 +0100

    ft: add RegistrationsController#-new-create and form for new

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,24 @@
+# frozen_literal_string: true
+
+# Controller in charge of new registrations
 class RegistrationsController < ApplicationController
   def new
+    @registration = Registration.new
+  end
+
+  def create
+    @registration = Registration.new(strong_params)
+
+    if Registrations::CreateService.call(@registration)
+      redirect_to root_path, notice: 'Succesfully created a registration'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:registration).permit(:email, :name, :bio, :phone)
   end
 end
