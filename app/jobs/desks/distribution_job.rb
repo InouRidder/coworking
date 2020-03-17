5948045c9ac1f392a68e3fbfe110a0440fff3cd8
Inou Ridder <inouridder@gmail.com>
2020-03-17 10:44:06 +0100

    refactor: improve test speed quality by reducing query load

--- app/jobs/desks/distribution_job.rb
+++ app/jobs/desks/distribution_job.rb
@@ -6,11 +6,9 @@ module Desks
     queue_as :default
 
     def perform
-      # I realise this is heavy on the DB, but seems the least prone to bug
-      while Desk.any_available? && Request.confirmed.any?
-        Request.confirmed.in_waiting_list_order.each do |request|
-          request.accept!
-        end
+      Request.confirmed.in_waiting_list_order.each do |request|
+        request.accept! # will simply fail if there no is desk available
+        break unless Desk.any_available?
       end
     end
   end
